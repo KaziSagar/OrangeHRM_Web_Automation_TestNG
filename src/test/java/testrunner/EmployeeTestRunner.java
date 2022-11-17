7f02949427d4c02c19e9a735e9e9fb9f64c94b41
KaziSagar <kazi.sagar@northsouth.edu>
2022-11-17 07:27:51 +0600

    intial commit - upto employee login.

---
+++ src/test/java/testrunner/EmployeeTestRunner.java
@@ -0,0 +1,196 @@
+package testrunner;
+
+import org.json.simple.JSONObject;
+import org.json.simple.parser.ParseException;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+import pages.DashboardPage;
+import pages.EmployeePage;
+import pages.LoginPage;
+import setup.Setup;
+import utils.Utils;
+
+import java.io.IOException;
+import java.time.Duration;
+import java.util.List;
+
+public class EmployeeTestRunner extends Setup {
+    DashboardPage dashboardPage;
+
+    @BeforeTest
+    public void doLogin(){
+        driver.get("https://opensource-demo.orangehrmlive.com");
+        LoginPage loginPage = new LoginPage(driver);
+        String adminUser ="admin";
+        String adminPassword = "admin123";
+        loginPage.doLogin(adminUser, adminPassword);
+    }
+
+    @Test(priority = 1, description = "Check if user exists", enabled = false)
+    public void checkIfUserExists() throws IOException, ParseException, InterruptedException {
+        dashboardPage = new DashboardPage(driver);
+        //Thread.sleep(5000);
+        dashboardPage.PIM.get(1).click();
+        EmployeePage employeePage = new EmployeePage(driver);
+        employeePage.btnAddEmployee.get(2).click();
+        Thread.sleep(3000);
+        employeePage.toggleButton.click();
+        List data = Utils.readJSONArray("./src/test/resources/Users.json");
+        JSONObject userObj = (JSONObject) data.get(data.size() - 1);
+        String existingUserName = (String) userObj.get("userName");
+        String validationMessageActual = employeePage.checkIfUserExists(existingUserName);
+        String validationMessageExpected = "Username already exists";
+        Assert.assertTrue(validationMessageActual.contains(validationMessageExpected));
+    }
+
+//    @Test(priority = 2, description = "Create new employee")
+//    public void create2Employees() throws InterruptedException, IOException, ParseException {
+//
+//
+//        for(int i=0; i<2; i++){
+//
+//            dashboardPage = new DashboardPage(driver);
+//            //Thread.sleep(5000);
+//            dashboardPage.PIM.get(1).click();
+//
+//            EmployeePage employeePage = new EmployeePage(driver);
+//
+//            Utils utils = new Utils();
+//            utils.generateRandomData();
+//            String firstName = utils.getFirstname();
+//            String lastName = utils.getLastname();
+//            int randomId = Utils.generateRandomNumber(1000, 9000);
+//            String userName = utils.getFirstname() + randomId;
+//            String password = "P@ssword123";
+//            String conFirmPassword = password;
+//
+//            employeePage.btnAddEmployee.get(2).click();
+//            Thread.sleep(3000);
+//            employeePage.toggleButton.click();
+//
+//            //employeePage.txtUserCreds.get(5).clear();
+//            employeePage.createEmployee(firstName, lastName, userName, password, conFirmPassword);
+//
+//            List <WebElement> headerTitle = driver.findElements(By.className("orangehrm-main-title"));
+//            Assert.assertTrue(headerTitle.get(0).isDisplayed());
+//
+//            utils.waitForElement(driver, headerTitle.get(0), 50);
+//
+//            if(headerTitle.get(0).isDisplayed()){
+//                utils.saveJsonList(userName, password);
+//            }
+//        }
+//    }
+
+
+
+
+
+
+
+
+
+
+
+    @Test(priority = 2, description = "Create new employee")
+    public void create2Employees() throws InterruptedException, IOException, ParseException {
+
+
+        for(int i=0; i<2; i++){
+
+            dashboardPage = new DashboardPage(driver);
+            //Thread.sleep(5000);
+            dashboardPage.PIM.get(1).click();
+
+            EmployeePage employeePage = new EmployeePage(driver);
+
+            Utils utils = new Utils();
+            utils.generateRandomData();
+            String firstName = utils.getFirstname();
+            String lastName = utils.getLastname();
+            int randomId = Utils.generateRandomNumber(1000, 9000);
+            String userName = utils.getFirstname() + randomId;
+            String password = "P@ssword123";
+            String conFirmPassword = password;
+            String employeeID = String.valueOf(randomId);
+
+            employeePage.btnAddEmployee.get(2).click();
+            Thread.sleep(3000);
+
+
+
+            //employeePage.txtUserCreds.get(4).clear();
+
+
+
+            employeePage.toggleButton.click();
+            //employeePage.employeeID.clear();
+
+            //employeePage.txtUserCreds.get(5).clear();
+            employeePage.createEmployee(firstName, lastName, employeeID, userName, password, conFirmPassword);
+
+            List <WebElement> headerTitle = driver.findElements(By.className("orangehrm-main-title"));
+            Assert.assertTrue(headerTitle.get(0).isDisplayed());
+
+            utils.waitForElement(driver, headerTitle.get(0), 50);
+
+            if(headerTitle.get(0).isDisplayed()){
+                utils.saveJsonList(userName, password, employeeID);
+            }
+        }
+    }
+
+
+
+
+
+    @Test(priority = 3, description = "Search employees by ID")
+    public void searchEmployeesByID() throws InterruptedException, IOException, ParseException {
+
+        dashboardPage = new DashboardPage(driver);
+        dashboardPage.PIM.get(1).click();
+
+        for(int i=0; i<2; i++){
+
+            List data = Utils.readJSONArray("./src/test/resources/Users.json");
+            JSONObject userObj = (JSONObject) data.get(i);
+            String userID = (String) userObj.get("userID");
+            dashboardPage.searchBar.get(1).click();
+            dashboardPage.searchBar.get(1).sendKeys(userID);
+            dashboardPage.btnSubmit.click();
+            Thread.sleep(2000);
+
+
+            String expectedResult = "Record Found";
+            String actualResult = dashboardPage.recordsFound.get(14).getText();
+            Assert.assertTrue(actualResult.contains(expectedResult));
+
+
+            dashboardPage.searchBar.get(1).click();
+
+            Thread.sleep(1000);
+            dashboardPage.searchBar.get(1).sendKeys(Keys.CONTROL, "A");
+
+            //Thread.sleep(1000);
+            dashboardPage.searchBar.get(1).sendKeys(Keys.BACK_SPACE);
+            //Thread.sleep(1000);
+        }
+    }
+
+    @AfterTest
+    public void logout() throws InterruptedException {
+        DashboardPage dashboardPage = new DashboardPage(driver);
+        dashboardPage.btnProfileIcon.click();
+        Thread.sleep(1000);
+        driver.findElement(By.partialLinkText("Logout")).click();
+    }
+}
---
+++ src/test/java/testrunner/LoginTestRunner.java
@@ -0,0 +1,124 @@
+package testrunner;
+
+import io.qameta.allure.Allure;
+import org.openqa.selenium.*;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import pages.DashboardPage;
+import pages.LoginPage;
+import setup.Setup;
+import utils.Utils;
+
+import java.util.List;
+
+public class LoginTestRunner extends Setup {
+
+    LoginPage loginPage;
+    DashboardPage dashboardPage;
+
+    @Test(priority = 1, description = "User cannot login with wrong creds")
+    public void doLoginWithWrongCreds(){
+        driver.get("https://opensource-demo.orangehrmlive.com");
+        loginPage = new LoginPage(driver);
+        loginPage.doLogin("wronguser", "password");
+        String validationMessageActual = driver.findElement(By.className("oxd-alert-content-text")).getText();
+        String validationMessageExpected = "Invalid credentials";
+        Assert.assertTrue(validationMessageActual.contains(validationMessageExpected));
+        Allure.description("User Cannot run with wrong creds");
+    }
+
+    @Test(priority = 2, description = "Admin can login successfully")
+    public void doLogin(){
+        //driver.get("https://opensource-demo.orangehrmlive.com");
+        loginPage = new LoginPage(driver);
+        loginPage.doLogin("admin", "admin123");
+
+        String urlActual = driver.getCurrentUrl();
+        String urlExpected = "index";
+        Assert.assertTrue(urlActual.contains(urlExpected));
+    }
+
+    @Test(priority = 3, description = "Admin profile image showing")
+    public void isProfileImageExists(){
+        dashboardPage = new DashboardPage(driver);
+       //WebElement imgProfile = driver.findElement(By.className("oxd-userdropdown-img"));
+        Assert.assertTrue(dashboardPage.imgProfile.isDisplayed());
+    }
+
+    @Test(priority = 4, description = "Dashboard in the URL")
+    public void isURLExists(){
+        dashboardPage = new DashboardPage(driver);
+        //WebElement imgProfile = driver.findElement(By.className("oxd-userdropdown-img"));
+        String expectedURL = "dashboard";
+        String actualURL = driver.getCurrentUrl();
+        //Assert.assertTrue(dashboardPage.imgProfile.isDisplayed());
+        Assert.assertTrue(actualURL.contains(expectedURL));
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+    @Test(priority = 4, description = "Select employee status", enabled = false)
+    public void selectEmploymentStatus() throws InterruptedException {
+        dashboardPage.PIM.get(1).click();
+        Thread.sleep(2000);
+        dashboardPage.dropDowns.get(0).click();
+        dashboardPage.dropDowns.get(0).sendKeys(Keys.ARROW_DOWN);
+        dashboardPage.dropDowns.get(0).sendKeys(Keys.ARROW_DOWN);
+        dashboardPage.dropDowns.get(0).sendKeys(Keys.ENTER);
+        dashboardPage.btnSubmit.click();
+
+        Thread.sleep(3000);
+
+        WebElement txtData = dashboardPage.recordsFound.get(14);
+        String dataActual = txtData.getText();
+        String dataExpected = "Records Found";
+        Assert.assertTrue(dataActual.contains(dataExpected));
+    }
+
+    @Test(priority = 5, description = "Showing employee list", enabled = false)
+    public void listEmployee() {
+        Utils.doScroll(driver);
+
+        WebElement table= driver.findElement(By.className("oxd-table-body"));
+        List<WebElement> allRows= table.findElements(By.cssSelector("[role=row]"));
+        for (WebElement row:allRows) {
+            List<WebElement> cells= row.findElements(By.cssSelector("[role=cell]"));
+            System.out.println(cells.get(5).getText());
+            Assert.assertTrue(cells.get(5).getText().contains("Full-Time Contract"));
+
+        }
+        //Allure.description("Employee list showing properly");
+    }
+
+    @Test(priority = 6, description = "Showing no employee data if not in database", enabled = false)
+    public void noEmployeeData() throws InterruptedException {
+
+        dashboardPage = new DashboardPage(driver);
+        dashboardPage.PIM.get(1).click();
+        Thread.sleep(2000);
+        dashboardPage.dropDowns.get(0).click();
+        dashboardPage.dropDowns.get(0).sendKeys(Keys.ARROW_DOWN);
+        dashboardPage.dropDowns.get(0).sendKeys(Keys.ARROW_DOWN);
+        dashboardPage.dropDowns.get(0).sendKeys(Keys.ARROW_DOWN);
+        dashboardPage.dropDowns.get(0).sendKeys(Keys.ARROW_DOWN);
+        dashboardPage.dropDowns.get(0).sendKeys(Keys.ARROW_DOWN);
+        dashboardPage.dropDowns.get(0).sendKeys(Keys.ARROW_DOWN);
+        dashboardPage.dropDowns.get(0).sendKeys(Keys.ENTER);
+        Thread.sleep(2000);
+        dashboardPage.btnSubmit.click();
+
+        List <WebElement> records = driver.findElements(By.className("oxd-text--span"));
+        String dataStatusActual = records.get(11).getText();
+        String dataStatusExpected = "No Records Found";
+        Assert.assertEquals(dataStatusActual, dataStatusExpected);
+    }
+}
